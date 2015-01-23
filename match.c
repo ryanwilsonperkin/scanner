f23bcedc5e532b52790e79cdc2522a554d1ddb24
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2015-01-22 23:57:47 -0500

    Limited version of match_real

--- match.c
+++ match.c
@@ -125,8 +125,82 @@ char match_oct(char *str)
 
 char match_real(char *str)
 {
-        char match = 0;
-        return match;
+        int i;
+        int length = strlen(str);
+        char c;
+        char state = 0;
+        const char accept_states[2] = {5, 7};
+        for(i = 0; i < length; i++) {
+                c = str[i];
+                switch (state) {
+                case 0:
+                        if (c == '-') {
+                                state = 1;
+                        } else if (c == '0') {
+                                state = 2;
+                        } else if (c >= '1' && c <= '9') {
+                                state = 3;
+                        } else {
+                                return 0;
+                        }
+                        break;
+                case 1:
+                        if (c == '0') {
+                                state = 2;
+                        } else if (c >= '1' && c <= '9') {
+                                state = 3;
+                        } else {
+                                return 0;
+                        }
+                        break;
+                case 2:
+                        if (c == '.') {
+                                state = 4;
+                        } else {
+                                return 0;
+                        }
+                        break;
+                case 3:
+                        if (c == '.') {
+                                state = 4;
+                        } else if (!(c >= '0' && c <= '9')) {
+                                return 0;
+                        }
+                        break;
+                case 4:
+                        if (c >= '0' && c<= '9') {
+                                state = 5;
+                        } else {
+                                return 0;
+                        }
+                        break;
+                case 5:
+                        if (c == 'e' || c == 'E') {
+                                state = 6;
+                        } else if (!(c >= '0' && c <= '9')) {
+                                return 0;
+                        }
+                        break;
+                case 6:
+                        if (c >= '1' && c <= '9') {
+                                state = 7;
+                        } else {
+                                return 0;
+                        }
+                        break;
+                case 7:
+                        if (!(c >= '0' && c <= '9')) {
+                                return 0;
+                        }
+                        break;
+                }
+        }
+        for(i = 0; i < 2; i++) {
+                if (state == accept_states[i]) {
+                        return 1;
+                }
+        }
+        return 0;
 }
 
 char match_lparen(char *str)
