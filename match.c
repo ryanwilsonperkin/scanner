84ee95100b4281dcc38b6507ba9c9682185f0754
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2015-01-22 22:42:14 -0500

    Implement INT matching DFA.

--- match.c
+++ match.c
@@ -1,7 +1,49 @@
+#include <string.h>
+
 char match_int(char *str)
 {
-        char match = 0;
-        return match;
+        int i;
+        int length = strlen(str);
+        char state = 0;
+        const char accept_states[2] = {2, 3};
+        for(i = 0; i < length; i++) {
+                switch (state) {
+                case 0:
+                        if (str[i] == '-') {
+                                state = 1;
+                        } else if (str[i] == '0') {
+                                state = 2;
+                        } else if (str[i] >= '0' && str[i] <= '9') {
+                                state = 3;
+                        } else {
+                                return 0;
+                        }
+                        break;
+                case 1:
+                        if (str[i] == '0') {
+                                state = 2;
+                        } else if (str[i] >= '0' && str[i] <= '9') {
+                                state = 3;
+                        } else {
+                                return 0;
+                        }
+                        break;
+                case 2:
+                        return 0;
+                        break;
+                case 3:
+                        if (!(str[i] >= '0' && str[i] <= '9')) {
+                                return 0;
+                        }
+                        break;
+                }
+        }
+        for(i = 0; i < 2; i++) {
+                if (state == accept_states[i]) {
+                        return 1;
+                }
+        }
+        return 0;
 }
 
 char match_hex(char *str)
